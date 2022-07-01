c009fc60bf9053c768955416d64a9726694f9c00
Snehasis Sasmal <snehasis@Snehasiss-MacBook-Pro.local>
2022-07-01 14:50:37 +0530

    modified app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/I have chnaged/);
+  const linkElement = screen.getByText(/I have changed on the feature branch/);
   expect(linkElement).toBeInTheDocument();
 });
